f3a0e23867e324f8a9c44f928975a301302cda5a
Harsimran Kaur <infamously13@gmail.com>
2022-09-19 23:35:56 -0400

    adding sign-in component

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,10 +1,11 @@
 // import { useEffect } from 'react';
-import { signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect} from '../../utils/firebase/firebase.utils'
+
 // import {getRedirectResult} from 'firebase/auth'
 // import {auth} from '../../utils/firebase/firebase.utils'
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
-const SignIn = () => {
+const Authentication = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     // useEffect(async () => {
     //     const response = await getRedirectResult(auth);
@@ -13,18 +14,15 @@ const SignIn = () => {
     //     }
     // }, [])
 
-    const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-    }
+
 
     return (
         <div>
             <h1> Sign In Page</h1>
-            <button onClick={logGoogleUser}> Sign in with Google Popup</button>
+            <SignInForm />
             <SignUpForm />
         </div>
     );
 };
 
-export default SignIn;
+export default Authentication;
