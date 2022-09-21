a7c6fa939383a1bcf94530674ab6be9834355224
Harsimran Kaur <infamously13@gmail.com>
2022-09-20 20:44:33 -0400

    Added CartIcon and CartDropdown components

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,6 +4,8 @@ import { Fragment, useContext } from 'react';
 import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 
 import './navigation.styles.scss'
 
@@ -30,8 +32,9 @@ const Navigation = () => {
                         <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>) : (
                         <Link className='nav-link' to='/auth'>SIGN IN</Link>)
                     }
-
+                    <CartIcon />
                 </div>
+                <CartDropdown />
             </div>
             <Outlet />
         </Fragment>
