2506f6701fa98e4f068fafe632487c2ea7191e1b
Harsimran Kaur <infamously13@gmail.com>
2022-09-25 23:32:43 -0400

    Displaying empty cart message

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -14,8 +14,8 @@ const CartDropdown = () => {
     }
     if (cartCount===0){
         return (
-            <div className='cart-dropdown-container empty-message'>
-                Your cart is empty!
+            <div className='cart-dropdown-container'>
+               <div className='empty-message'>Your cart is empty!</div> 
             </div>
         )
     }
