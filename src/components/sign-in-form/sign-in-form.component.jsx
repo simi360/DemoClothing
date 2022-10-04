472c596daf1a64824d3fc758224b3c2350180389
Harsimran Kaur <infamously13@gmail.com>
2022-10-03 21:14:36 -0400

    Converting scss files to styled-component

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { signInAuthUserWithEmailAndPassword, signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
 import './sign-in-form.styles.scss'
-import Button from '../button/button.component'
+import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component'
 
 
 const defaultFormFields = {
@@ -72,7 +72,7 @@ const SignInForm = () => {
             />
             <div className="buttons-container">
                 <Button type='submit'>Sign In </Button>
-                <Button type='button' buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
+                <Button buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>Google Sign In</Button>
             </div>
             </form>
         </div>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -8,7 +8,7 @@ import { signOutUser } from '../../utils/firebase/firebase.utils';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
-import './navigation.styles.scss'
+import { NavLink, NavLinks, NavigationContainer, LogoContainer } from './navigation.styles'
 
 const Navigation = () => {
     const {currentUser} = useContext(UserContext);
@@ -20,23 +20,23 @@ const Navigation = () => {
 
     return (
         <Fragment>
-            <div className='navigation'>
-                <Link className='logo-container' to='/'>
+            <NavigationContainer>
+                <LogoContainer to='/'>
                     <CrwnLogo className='logo'/>
-                </Link>
-                <div className='nav-links-container'>
-                    <Link className='nav-link' to='/shop'>
+                </LogoContainer>
+                <NavLinks>
+                    <NavLink to='/shop'>
                         SHOP
-                    </Link>
+                    </NavLink>
                     {
                         currentUser ? (
-                        <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>) : (
-                        <Link className='nav-link' to='/auth'>SIGN IN</Link>)
+                        <NavLink as='span' onClick={signOutUser}>SIGN OUT</NavLink>) : (
+                        <NavLink to='/auth'>SIGN IN</NavLink>)
                     }
                     <CartIcon />
-                </div>
+                </NavLinks>
                 {isCartOpen && <CartDropdown />}
-            </div>
+            </NavigationContainer>
             <Outlet />
         </Fragment>
     );
