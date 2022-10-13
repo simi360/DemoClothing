deb5e7dae4fede12f2240b8a3fc248cf09709da5
Harsimran Kaur <infamously13@gmail.com>
2022-10-12 20:15:16 -0400

    removed unnecessary imported libraries

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect, useReducer } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 import {onAuthStateChangedListener, createUserDocumentFromAuth} from '../utils/firebase/firebase.utils'
 
 import { createAction } from "../utils/reducer/reducer.utils";
