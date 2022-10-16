d708601e26fa9bdcdf1f93056010035b44964753
Harsimran Kaur <infamously13@gmail.com>
2022-10-16 17:55:52 -0400

    adding redux functionalities

---
+++ src/store/store.js
@@ -0,0 +1,5 @@
+import {compose, createStore, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+import { rootReducer } from './root-reducer';
+
+export const store = createStore(rootReducer);
