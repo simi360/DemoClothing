b1964b513525c1937321f44c3aa007e82426b83c
Harsimran Kaur <infamously13@gmail.com>
2022-10-18 19:05:56 -0400

    adding redux in the project

--- src/store/store.js
+++ src/store/store.js
@@ -2,4 +2,8 @@ import {compose, createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
-export const store = createStore(rootReducer);
+const middleWares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
