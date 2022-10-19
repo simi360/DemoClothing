8e7e09aafb4f514fe8e3747eff67f2b6f2d0f75f
Harsimran Kaur <infamously13@gmail.com>
2022-10-19 14:31:38 -0400

    creating initial categories reducer and updating category map to generate from selector

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 
 import App from './App';
 
-import { CategoriesProvider } from './contexts/categories.context'
+
 import { CartProvider } from './contexts/cart.context'; 
 import { store } from './store/store';
 import reportWebVitals from './reportWebVitals';
@@ -18,11 +18,11 @@ root.render(
    <React.StrictMode>
       <Provider store={store}>  
          <BrowserRouter>
-            <CategoriesProvider>
+            
                <CartProvider>
                   <App />
                </CartProvider>
-            </CategoriesProvider>
+            
          </BrowserRouter>
       </Provider>
    </React.StrictMode>
