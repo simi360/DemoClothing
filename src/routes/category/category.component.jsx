a3ba1615a1b1b250e9d402330b794094ddbdfa45
Harsimran Kaur <infamously13@gmail.com>
2022-10-19 18:32:08 -0400

    updating category map to generate from selector

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import ProductCard from "../../components/product-card/product-card.component";
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import { selectCategories } from "../../store/categories/category.selector";
 
 import {CategoryTitle, CategoryContainer} from './category.styles.jsx';
 
 const Category = () => {
     const {category} = useParams();
-    const categoriesMap = useSelector(selectCategoriesMap);
+    const categoriesMap = useSelector(selectCategories);
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect (() => {
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import { Routes, Route} from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategoriesMap } from '../../store/categories/category.action';
+import { setCategories } from '../../store/categories/category.action';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component'
@@ -13,9 +13,9 @@ const Shop = () => {
 
     useEffect(() => {
         const getCategoriesMap = async () => {
-            const categoryMap = await getCategoriesAndDocuments();
-            // console.log(categoryMap);
-            dispatch(setCategoriesMap(categoryMap));
+            const categoriesArray = await getCategoriesAndDocuments('categories');
+            // console.log(categoriesArray);
+            dispatch(setCategories(categoriesArray));
         }
         getCategoriesMap();
     }, []);
