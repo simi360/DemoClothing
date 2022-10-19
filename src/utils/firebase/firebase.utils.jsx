3d8e8530ba2fc8b6f6ec304276346de94e893991
Harsimran Kaur <infamously13@gmail.com>
2022-10-19 18:32:03 -0400

    updating category map to generate from selector

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -61,13 +61,11 @@ const firebaseConfig = {
     const q = query(collectionRef);
 
     const querySnapshot = await getDocs(q);
-    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
-      const {title, items} = docSnapshot.data();
-      acc[title.toLowerCase()] = items;
-      return acc;
-    }, {});
+    return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
+    
 
-    return categoryMap;
+
+    // return categoryMap;
   }
 
   export const createUserDocumentFromAuth = async (userAuth, additionalInformation={}) => {
