09624a584b71233619601fd88acad9e62ae77a92
Harsimran Kaur <infamously13@gmail.com>
2022-10-25 14:14:57 -0400

    implementing redux-thunk to do async actions

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,8 +1,7 @@
 import { Routes, Route} from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/category.action';
+import { fetchCategoriesAsync } from '../../store/categories/category.action';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component'
@@ -12,12 +11,7 @@ const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const getCategoriesMap = async () => {
-            const categoriesArray = await getCategoriesAndDocuments('categories');
-            // console.log(categoriesArray);
-            dispatch(setCategories(categoriesArray));
-        }
-        getCategoriesMap();
+            dispatch(fetchCategoriesAsync());
     }, []);
 
     return (
