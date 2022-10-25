bf1282f05994b3646ef64eab5fd5227a8c9c20b3
Harsimran Kaur <infamously13@gmail.com>
2022-10-25 08:51:42 -0400

    Imported redux-thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,10 @@
 import {compose, createStore, applyMiddleware} from 'redux';
 // importing self-made logger, can also import logger from 'redux-logger';
-import { loggerMiddleware } from './middleware/logger'
+import { loggerMiddleware } from './middleware/logger';
 import { rootReducer } from './root-reducer';
 import {persistStore, persistReducer} from 'redux-persist';
-import storage from 'redux-persist/lib/storage'
-
+import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
 
 
 const persistConfig = {
