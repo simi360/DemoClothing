249884516235e064031455cececacd17e829bd08
Harsimran Kaur <infamously13@gmail.com>
2022-10-27 16:41:05 -0400

    Adding payment-intent

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -14,16 +14,43 @@ const PaymentForm = () => {
         if(!stripe || !elements){
             return;
         }
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({amount: 10000})
+        }).then(res=> res.json());
 
-        
+        const clientSecret = response.paymentIntent.clint_secret;
+        //OR can also do like this -> const {paymentIntent: {client_secret}} = response;
+
+        console.log(clientSecret);
+
+        const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Harsimran Kaur'
+                }
+            }
+        })
+
+        if(paymentResult.error) {
+            alert(paymentResult.error)
+        } else {
+            if(paymentResult.paymentIntent.status === 'succeeded') {
+                alert('Payment successful')
+            }
+        }
     }
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer >
                 <h2>Credit Card Payment: </h2>
                 <CardElement />
-                <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
+                <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onSubmit={paymentHandler}>Pay Now</Button>
             </FormContainer>
         </PaymentFormContainer>
     )
