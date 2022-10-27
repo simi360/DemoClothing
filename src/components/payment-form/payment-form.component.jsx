e19e7adb6450bf70666ab31263fbc7562907fe18
Harsimran Kaur <infamously13@gmail.com>
2022-10-27 18:12:19 -0400

    setting up netlify function and stripe payment processing with card element

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,12 +1,19 @@
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
-import Button, { BUTTON_TYPE_CLASSES} from "../button/button.component";
+import { useState } from 'react';
+import { BUTTON_TYPE_CLASSES} from "../button/button.component";
+import { useSelector } from "react-redux";
+import { selectCartTotal } from '../../store/cart/cart.selector';
+import { selectCurrentUser } from '../../store/user/user.selector';
 
-import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
+import { PaymentFormContainer, FormContainer, PaymentButton } from "./payment-form.styles";
 
 
 const PaymentForm = () => {
     const elements = useElements();
     const stripe = useStripe();
+    const amount = useSelector(selectCartTotal);
+    const currentUser = useSelector(selectCurrentUser);
+    const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
     const paymentHandler = async (e) => {
         e.preventDefault();
@@ -14,27 +21,31 @@ const PaymentForm = () => {
         if(!stripe || !elements){
             return;
         }
+        setIsProcessingPayment(true);
         const response = await fetch('/.netlify/functions/create-payment-intent', {
             method: 'post',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({amount: 10000})
+            body: JSON.stringify({amount: amount*100})
         }).then(res=> res.json());
 
         const clientSecret = response.paymentIntent.clint_secret;
+        // console.log("Debugging log ~ file: payment-form.component.jsx ~ line 32 ~ paymentHandler ~ clientSecret", clientSecret);
         //OR can also do like this -> const {paymentIntent: {client_secret}} = response;
 
-        console.log(clientSecret);
+        
 
         const paymentResult = await stripe.confirmCardPayment(clientSecret, {
             payment_method: {
                 card: elements.getElement(CardElement),
                 billing_details: {
-                    name: 'Harsimran Kaur'
+                    name: currentUser ? currentUser.displayName : 'Guest',
                 }
             }
         })
+
+        setIsProcessingPayment(false);
 
         if(paymentResult.error) {
             alert(paymentResult.error)
@@ -45,12 +56,19 @@ const PaymentForm = () => {
         }
     }
 
+    // const payment2Handler = () => {
+    //     console.log("Debugging log ~ file: payment-form.component.jsx ~ line 58 ~ payment2Handler ~ payment2Handler", payment2Handler);
+    // }
+    
     return (
         <PaymentFormContainer>
-            <FormContainer >
+            <FormContainer onSubmit={paymentHandler} >
                 <h2>Credit Card Payment: </h2>
                 <CardElement />
-                <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onSubmit={paymentHandler}>Pay Now</Button>
+                <PaymentButton 
+                isLoading={isProcessingPayment}
+                buttonType={BUTTON_TYPE_CLASSES.inverted}
+                >Pay Now</PaymentButton>
             </FormContainer>
         </PaymentFormContainer>
     )
