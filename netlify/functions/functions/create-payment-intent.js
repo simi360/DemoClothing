e26f10edc30116afbe6c36db3c19c3c239bae1f1
Harsimran Kaur <infamously13@gmail.com>
2022-10-28 13:48:25 -0400

    removing unnecessary console.log

--- netlify/functions/functions/create-payment-intent.js
+++ netlify/functions/functions/create-payment-intent.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
+  console.log(event.path);
   try {
     const { amount } = JSON.parse(event.body);
 
